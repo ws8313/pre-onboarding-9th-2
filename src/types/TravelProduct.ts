b7c7ca5d123d529b62ea9d7539114a0e4dd2c770
Wooseong Kim <kimwooseong8313@gmail.com>
2023-03-08 16:39:53 +0900

    feat: TravelProduct 타입

---
+++ src/types/TravelProduct.ts
@@ -0,0 +1,12 @@
+type TravelProduct = {
+  idx: number;
+  name: string;
+  mainImage: string;
+  description: string;
+  spaceCategory: string;
+  price: number;
+  maximumPurchases: number;
+  registrationDate: string;
+};
+
+export default TravelProduct;
