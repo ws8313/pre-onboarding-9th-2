6efca5c5a8437f84a8e598037d6be79d0be0b572
Wooseong Kim <kimwooseong8313@gmail.com>
2023-03-08 17:52:22 +0900

    feat: main 페이지 상품 목록 보여주기

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,3 +1,22 @@
+import travelProducts from "../../data/travelProducts";
+import TravelProduct from "../../types/TravelProduct";
+import { TravelList } from "../../components/TravelList";
+import { Container, Flex, Grid, Heading } from "@chakra-ui/react";
+
 export const MainPage = () => {
-  return <>MainPage</>;
+  const products: TravelProduct[] = travelProducts;
+
+  return (
+    <Container>
+      <Flex justifyContent="center">
+        <Heading marginBottom={70}>여행 상품</Heading>
+      </Flex>
+
+      <Grid templateColumns={"repeat(2, 1fr)"} gap={4}>
+        {products.map((el, idx) => {
+          return <TravelList key={idx} travelProduct={el} />;
+        })}
+      </Grid>
+    </Container>
+  );
 };
